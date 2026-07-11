e8c90c65b0328c77604234534367a342cd1be3ae
coulsonzero <coulson2021@163.com>
2026-07-12 01:59:03 +0800

    update

--- src/components/home/BackgroundImg.tsx
+++ src/components/home/BackgroundImg.tsx
@@ -4,10 +4,13 @@ export default function BackgroundImg() {
   return (
     <div className="background-wrapper">
         <div className="wrapper-images">
+            <div className="wrapper-img bg-0"></div>
             <div className="wrapper-img bg-1"></div>
             <div className="wrapper-img bg-2"></div>
             <div className="wrapper-img bg-3"></div>
             <div className="wrapper-img bg-4"></div>
+            <div className="wrapper-img bg-5"></div>
+            <div className="wrapper-img bg-6"></div>
         </div>
         <div className="wrapper-blur"></div>
         <div className="wrapper-color"></div>
