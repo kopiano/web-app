a43d056ba08f1cbadab70158e72972da9d62be36
coulsonzero <coulson2021@163.com>
2026-07-21 21:12:28 +0800

    [commit]: fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [react()],
   // base: "/web-app/",    // github page
-  base: "/",               // if custom domain
+  // base: "/",               // if custom domain
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
